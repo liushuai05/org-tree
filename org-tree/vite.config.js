e8e5fab2ae9b15ec1b683c5c7ec046a78f7dbfb5
Your Name <your_email@example.com>
2025-10-23 12:00:07 +0800

    1.简化代码,之前大佬写的那个js太复杂了,用组件递归调用实现  2.去掉vue-demi依赖,以最简单的原生方案兼容vue2和vue3

--- org-tree/vite.config.js
+++ org-tree/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue2 from '@vitejs/plugin-vue2'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue2()],
   build: {
     outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
@@ -11,7 +11,7 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/index.js'),
       name: 'OrgTree',
       fileName: (format) => `org-tree.${format}.js`, // 简化文件名
-      formats: ['es', 'umd']
+      formats: ['es', 'umd', 'cjs']
     },
     rollupOptions: {
       external: ['vue', 'vue-demi'],
@@ -22,7 +22,9 @@ export default defineConfig({
         },
         // 配置 CSS 输出路径（相对 outDir）
         css: {
-          fileName: 'css/org-tree.css' // 输出到 dist/vue3/css
+          // 输出到 outDir 下的 css 子目录（即 dist/vue2/css）
+          // 也可直接输出到 outDir 根目录：fileName: 'org-tree.css'
+          fileName: 'css/org-tree.css'
         }
       }
     }
